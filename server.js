ce697563eae458d738e3d5120604054e158d9bc9
Austin Pratt <hargis32@gmail.com>
2022-01-24 16:16:43 -0600

    updated server.js

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ app.use('/api', api);
 app.use('/html', html);
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/', (req, res) => 
+  res.sendfile(path.join(__dirname, '/public/index.html'))
+);
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
