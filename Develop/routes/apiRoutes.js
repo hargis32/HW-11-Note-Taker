55167db1891c8b48f66eb0e9acf99433b6f0c676
Austin Pratt <hargis32@gmail.com>
2022-01-24 15:16:33 -0600

    apiRoutes.js has necessary routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,29 @@
 const fs = require("fs");
-const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const note = require('express').Router();
+const [readFromFile, readAndAppend, writeToFile] = require('../helpers/fsUtils');
 
 note.get('/api/notes', (req,res) => {
-    readFromFile('./db/db.json')
-})
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+note.post('/api/notes', (req,res) => {
+    console.log(req.body);
+
+    const {title, text} = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note successfully added!');
+    } else {
+        res.error("Note not added, try again!");
+    }
+});
+
+module.exports = note;
