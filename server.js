7e7d5b14556c2b494413722f1df8127ce0f97bc6
Austin Pratt <hargis32@gmail.com>
2022-01-27 12:55:00 -0600

    capitalized sendFile

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/notes', (req, res) =>
 );
 
 app.get('/', (req, res) => 
-  res.sendfile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
