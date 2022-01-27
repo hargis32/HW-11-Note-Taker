8af635c8a83b2dc5dbf7149ae05591e97c13a032
Austin Pratt <hargis32@gmail.com>
2022-01-27 12:53:50 -0600

    removed redundancies from server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/apiRoutes');
 const html = require('./routes/htmlRoutes');
-const {readFromFile, readAndAppend, writeToFile} = require('./helpers/fsUtils');
-const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.Port || 3001;
 
